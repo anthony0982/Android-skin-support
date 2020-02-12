d4ce7229843cb3d401d003fe9bd363aa0a3a833c
Anthony <ngwang.io@gmail.com>
2020-02-12 16:56:34 +0800

    support image tint

--- androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatImageHelper.java
+++ androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatImageHelper.java
@@ -1,21 +1,24 @@
 package skin.support.widget;
 
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import skin.support.R;
+import skin.support.content.res.SkinCompatResources;
 import skin.support.content.res.SkinCompatVectorResources;
 
 /**
  * Created by ximsfei on 2017/1/12.
  */
 public class SkinCompatImageHelper extends SkinCompatHelper {
-    private static final String TAG = SkinCompatImageHelper.class.getSimpleName();
     private final ImageView mView;
     private int mSrcResId = INVALID_ID;
     private int mSrcCompatResId = INVALID_ID;
+    private int mTintResId = INVALID_ID;
 
     public SkinCompatImageHelper(ImageView imageView) {
         mView = imageView;
@@ -26,6 +29,7 @@ public class SkinCompatImageHelper extends SkinCompatHelper {
         try {
             a = mView.getContext().obtainStyledAttributes(attrs, R.styleable.SkinCompatImageView, defStyleAttr, 0);
             mSrcResId = a.getResourceId(R.styleable.SkinCompatImageView_android_src, INVALID_ID);
+            mTintResId = a.getResourceId(R.styleable.SkinCompatImageView_android_tint, INVALID_ID);
             mSrcCompatResId = a.getResourceId(R.styleable.SkinCompatImageView_srcCompat, INVALID_ID);
         } finally {
             if (a != null) {
@@ -43,6 +47,7 @@ public class SkinCompatImageHelper extends SkinCompatHelper {
     @Override
     public void applySkin() {
         mSrcCompatResId = checkResourceId(mSrcCompatResId);
+        mTintResId = checkResourceId(mTintResId);
         if (mSrcCompatResId != INVALID_ID) {
             Drawable drawable = SkinCompatVectorResources.getDrawableCompat(mView.getContext(), mSrcCompatResId);
             if (drawable != null) {
@@ -56,6 +61,14 @@ public class SkinCompatImageHelper extends SkinCompatHelper {
             Drawable drawable = SkinCompatVectorResources.getDrawableCompat(mView.getContext(), mSrcResId);
             if (drawable != null) {
                 mView.setImageDrawable(drawable);
+            }
+        }
+        if (mTintResId != INVALID_ID) {
+            ColorStateList colorStateList = SkinCompatResources.getColorStateList(mView.getContext(), mTintResId);
+            if (colorStateList != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    mView.setImageTintList(colorStateList);
+                }
             }
         }
     }
--- androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatTextView.java
+++ androidx/skin-support-appcompat/src/main/java/skin/support/widget/SkinCompatTextView.java
@@ -43,7 +43,6 @@ public class SkinCompatTextView extends AppCompatTextView implements SkinCompatS
         }
     }
 
-
     @Override
     public void setTextAppearance(int resId) {
         setTextAppearance(getContext(), resId);
